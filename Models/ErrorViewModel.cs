be6492da9e6170ab683b7db5f886d7b256529d73
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-08-06 00:05:37 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jeux_Olympiques.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
