d3c7426b4e93cb9fae25cfd368189549552b532a
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-08-09 01:07:05 +0200

    F04 > INSCRIPTION : Ajout des nouveaux attributs pour User et réussite du test unitaire de génération de clé à l'inscription

---
+++ Areas/Identity/Data/Jeux_OlympiquesContext.cs
@@ -0,0 +1,22 @@
+﻿using Jeux_Olympiques.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jeux_Olympiques.Data;
+
+public class Jeux_OlympiquesContext : IdentityDbContext<Jeux_OlympiquesUser>
+{
+    public Jeux_OlympiquesContext(DbContextOptions<Jeux_OlympiquesContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Areas/Identity/Data/Jeux_OlympiquesUser.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+using System.Text;
+
+namespace Jeux_Olympiques.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Jeux_OlympiquesUser class
+public class Jeux_OlympiquesUser : IdentityUser
+{
+    [PersonalData]
+    public string? FirstName { get; set; }
+
+    [PersonalData]
+    public string? LastName { get; set; }
+
+    [PersonalData]
+    public string? AccountKey { get; private set; }
+
+    // Méthode pour générer la clé unique
+    public void GenerateAccountKey()
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var combinedValue = $"{Id}{FirstName}{LastName}{Email}";
+            var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedValue));
+            AccountKey = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
+
