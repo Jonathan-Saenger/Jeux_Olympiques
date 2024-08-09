f8e3cb4c44035de5f4617b7dce027ccfc1561a29
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-08-10 00:16:52 +0200

    F04 > INSCRIPTION : MàJ d'EF Core, modification ApplicationDbContext, migrations puis modification de la base de données

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Jeux_Olympiques.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Jeux_Olympiques.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Jeux_OlympiquesUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
