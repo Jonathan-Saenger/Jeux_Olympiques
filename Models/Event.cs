67211383f211ca6227a7501d7b1dba10f5e24628
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-09-04 23:03:37 +0200

    F01 > NAVIGATION : MàJ de la base de données, modification du lien vers les épreuves

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,7 +5,7 @@
         public int Id {  get; set; }
         public string? Entitled { get; set; }
         public string? Presentation { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
         public string? Site {  get; set; }
         public Offer? Offer { get; set; }
         public User? Created { get; set; }
--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? QrCode { get; set; }
-        public float Price { get; set; }
+        public decimal? Price { get; set; }
         public string? TicketKey { get; set; }
         public User? Buyer { get; set; }
         public Offer? Contains { get; set; }
