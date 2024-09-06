9e4bfdc5f43258efde822ac7421897d51b9aebec
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-09-07 00:44:00 +0200

    F09 > MISEàJOUR : Modifications visuelles de l'espace de gestion

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,12 +1,23 @@
-﻿namespace Jeux_Olympiques.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jeux_Olympiques.Models
 {
     public class Event
     {
         public int Id {  get; set; }
+
+        [Display(Name = "Sport")]
         public string? Entitled { get; set; }
+
         public string? Presentation { get; set; }
+
+        [Display(Name = "Date et heure")]
         public DateTime? Date { get; set; }
+
+        [Display(Name = "Lieu")]
         public string? Site {  get; set; }
+
+        [Display(Name = "Choix du ticket")]
         public Offer? Offer { get; set; }
         public User? Created { get; set; }
     }
