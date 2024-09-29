d771f58a2ca5bc09157963082ef9a1b80f6a227e
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-09-30 00:07:31 +0200

    F02 > EVENEMENTS: Implémentation du filtre de recherche (sans AJAX) dans la page Billetterie

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggleSearch');
+    const searchForm = document.getElementById('searchForm');
+
+    toggleButton.addEventListener('click', () => {
+        const { display } = searchForm.style;
+        searchForm.style.display = display === 'none' ? 'block' : 'none';
+        toggleButton.textContent = display === 'none'
+            ? 'Masquer le formulaire de recherche'
+            : 'Rechercher un évènement selon vos critères';
+    });
+});
