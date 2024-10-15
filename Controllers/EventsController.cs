96186b9da1ead7e85dd3ffd5c1464a0025b0cc5e
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-10-15 15:12:50 +0200

    F10 > VERIFICATION: Modification de la page ticket pour vérification. MàJ de la documentation des controllers Checkout et Events

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -27,6 +27,19 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // GET: Events/Details/5
+        /// <summary>
+        /// Afficher les détails d'un événement précis.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'événement à afficher.</param>
+        /// <returns>
+        /// La vue des détails de l'événement ou une réponse NotFound si l'événement n'existe pas.
+        /// </returns>
+        /// <remarks>
+        /// Vérifie si l'identifiant fourni est nul (renvoie une réponse NotFound).
+        /// Sinon, elle tente de récupérer l'événement à partir du contexte de la base de données, 
+        /// en incluant les offres associées à l'événement. Si l'événement est trouvé, la vue des 
+        /// détails de l'événement est renvoyée, sinon une réponse NotFound est renvoyée.
+        /// </remarks>
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -53,8 +66,21 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // POST: Events/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Créer un nouvel événement.
+        /// </summary>
+        /// <param name="@event">L'objet Event contenant les informations à enregistrer.</param>
+        /// <returns>
+        /// Redirige vers l'index des événements si la création est réussie 
+        /// </returns>
+        /// <remarks>
+        /// Cette méthode est protégée contre les attaques de surpostage en liant uniquement 
+        /// les propriétés spécifiées de l'objet Event. L'utilisateur doit être authentifié 
+        /// avec le rôle "Admin". L'événement est ajouté au contexte et les modifications sont 
+        /// enregistrées. Sinon, la vue est renvoyée avec l'objet Event pour permettre 
+        /// à l'utilisateur de corriger les erreurs.
+        /// Référence de .NET, consultez : http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// </remarks>
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
@@ -70,6 +96,18 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // GET: Events/Edit/5
+        /// <summary>
+        /// Affiche le formulaire d'édition d'un événement spécifique.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'événement à éditer.</param>
+        /// <returns>
+        /// La vue du formulaire d'édition de l'événement, ou une réponse NotFound si l'événement n'existe pas.
+        /// </returns>
+        /// <remarks>
+        /// L'utilisateur doit être authentifié avec le rôle "Admin". Cette méthode récupère l'événement 
+        /// correspondant à l'identifiant depuis la base de données. Si l'événement est trouvé, la vue 
+        /// du formulaire d'édition est renvoyée, sinon une réponse NotFound est également renvoyée.
+        /// </remarks>
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
@@ -87,8 +125,23 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // POST: Events/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Gère la requête POST pour mettre à jour un événement spécifique.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'événement à éditer.</param>
+        /// <param name="@event">L'objet Event contenant les informations mises à jour.</param>
+        /// <returns>
+        /// Redirige vers l'index des événements si la mise à jour est réussie, ou renvoie la vue avec 
+        /// le modèle en cas d'erreur de validation ou si l'événement n'existe pas.
+        /// </returns>
+        /// <remarks>
+        /// Cette méthode est protégée et nécessite également que l'utilisateur soit authentifié avec le rôle "Admin".
+        ///  Si l'identifiant fourni ne correspond pas à celui de l'événement dans le modèle, une réponse NotFound est renvoyée.
+        /// Si l'état du modèle est valide, la méthode tente de mettre à jour l'événement dans le contexte. En cas d'échec
+        ///  de la mise à jour due à une concurrence, elle vérifie si l'événement existe toujours. Si l'événement n'existe plus, 
+        /// une réponse NotFound est renvoyée, sinon une exception est relancée.
+        /// Pour plus de détails sur la protection contre le surpostage, consultez : http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// </remarks>
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
@@ -123,6 +176,22 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // GET: Events/Delete/5
+        /// <summary>
+        /// Gère la requête GET pour afficher la page de confirmation de suppression d'un événement spécifique.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'événement à supprimer.</param>
+        /// <returns>
+        /// La vue de confirmation de suppression de l'événement, ou une réponse NotFound si l'événement n'existe pas.
+        /// </returns>
+        /// <remarks>
+        /// Cette méthode nécessite que l'utilisateur soit authentifié avec le rôle "Admin". 
+        /// Elle commence par vérifier si l'identifiant fourni est nul. Si c'est le cas, 
+        /// elle renvoie une réponse NotFound. Ensuite, elle tente de récupérer l'événement 
+        /// correspondant à l'identifiant à partir du contexte de la base de données.
+        /// Si l'événement est trouvé, la vue de confirmation de suppression est renvoyée. 
+        /// Sinon, une réponse NotFound est également renvoyée, indiquant que l'événement 
+        /// à supprimer n'existe pas.
+        /// </remarks>
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
@@ -142,6 +211,23 @@ namespace Jeux_Olympiques.Controllers
         }
 
         // POST: Events/Delete/5
+        /// <summary>
+        /// Gère la requête POST pour confirmer la suppression d'un événement spécifique.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'événement à supprimer.</param>
+        /// <returns>
+        /// L'index des événements après la suppression
+        /// </returns>
+        /// <remarks>
+        /// Cette méthode est protégée contre les attaques CSRF (Cross-Site Request Forgery) 
+        /// Elle nécessite également que l'utilisateur soit authentifié avec le rôle "Admin".
+        /// Elle tente d'abord de trouver l'événement correspondant à l'identifiant 
+        /// fourni. Si l'événement est trouvé, il est supprimé du contexte. 
+        /// Quoi qu'il en soit, après l'opération, les changements sont enregistrés 
+        /// dans la base de données. La méthode redirige ensuite vers l'index des 
+        /// événements, que la suppression ait été effectuée ou que l'événement 
+        /// n'ait pas été trouvé.
+        /// </remarks>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
