a99040cdd98ca5cd79e57bfb318df63a03f600d9
Jonathan Saenger <jonathan.saenger@outlook.fr>
2024-10-24 00:23:11 +0200

    MAIN> CORRECTIONS: Amélioration de la page Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 using Jeux_Olympiques.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Jeux_Olympiques.Data;
+using SendGrid.Helpers.Mail;
 
 namespace Jeux_Olympiques.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var events = await _context.Events
+                .OrderByDescending(e => e.Date)
+                .Take(2)
+                .ToListAsync();
+
+            return View(events);
         }
 
         public IActionResult Privacy()
